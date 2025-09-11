3c4eaa8ed268ac8c6ca434a66f84faa3c5b26f4a
es-sai-fi <es.sai.fi24@gmail.com>
2025-09-10 19:11:38 -0500

    Changed project structure

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const ROOT = resolve(__dirname, "src");
+const PUBLIC_DIR = resolve(__dirname, "public");
+const OUT_DIR = resolve(__dirname, "dist");
+
 export default defineConfig({
+  root: ROOT,
+  publicDir: PUBLIC_DIR,
   build: {
+    outDir: OUT_DIR,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/views/index.html"),
+        main: resolve(ROOT, "index.html"),
+        about: resolve(ROOT, "about", "index.html"),
       },
     },
   },
